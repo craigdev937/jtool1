ac2f0a549dba6c1fc5ac52216732185176421d2f
Craig <indigomx9@gmail.com>
2021-11-16 21:01:41 -0500

    Finished the Routes, Models, and Controllers.

--- server/src/controllers/actorCon.js
+++ server/src/controllers/actorCon.js
@@ -18,7 +18,7 @@ async (req, res, next) => {
     }
 };
 
-export const GetAllActors = 
+export const FetchAllActors = 
 async (req, res, next) => {
     try {
         await Actor.find()
@@ -28,6 +28,44 @@ async (req, res, next) => {
         next(error);
     }
 };
+
+export const GetOneActor =
+async (req, res, next) => {
+    try {
+        await Actor.findById(req.params.id)
+        .then((actor) => res.status(200).json(actor));
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+        next(error);
+    }
+};
+
+export const UpdateActor =
+async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { firstName, lastName, age, info } = req.body;
+        await Actor.findByIdAndUpdate(id, {
+            firstName, lastName, age, info
+        })
+        .then(() => res.status(201).json("Actor Updated!"));
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+        next(error);
+    }
+};
+
+export const DeleteActor = 
+async (req, res, next) => {
+    try {
+        await Actor.findByIdAndDelete(req.params.id)
+        .then(() => res.status(201).json("Actor Deleted!"));
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+        next(error);
+    }
+};
+
 
 
 
--- server/src/routes/actorRt.js
+++ server/src/routes/actorRt.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { CreateActor, 
-    GetAllActors } from "../controllers/actorCon.js";
+import { CreateActor, DeleteActor, FetchAllActors, 
+    GetOneActor, UpdateActor } from "../controllers/actorCon.js";
 
 export const actorRt = express.Router();
-    actorRt.post("/create", CreateActor);
-    actorRt.get("/getall", GetAllActors);
+    actorRt.post("/actors", CreateActor);
+    actorRt.get("/actors", FetchAllActors);
+    actorRt.get("/actors/:id", GetOneActor);
+    actorRt.put("/actors/:id", UpdateActor);
+    actorRt.delete("/actors/:id", DeleteActor);
 
 
-
-
-    
